a923f6beaf9c3eb57d72050d810481c10cbb662e
Valentin Magaña <valentin.magana@adweb.mx>
2017-05-17 15:38:05 -0500

    Actualización

--- models/__init__.py
+++ models/__init__.py
@@ -5,8 +5,9 @@
 #
 # Este es el archivo que inicializa el paquete.
 # Aquí se importan cada uno de las clases que componen el paquete
-# En este caso hago una clase para los campos adicionales en crm (oportunidades) y partners (clientes) 
-# 
+# En este caso hago una clase para los campos adicionales en crm (oportunidades) y partners (clientes)
+#
 
 import partner_custom_fields
 import crm_custom_fields
+import sales_custom_fields
